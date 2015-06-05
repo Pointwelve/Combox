502d6b3a2dc0ccf4e10d945288fa61eac4765523
Ryne Cheow <rynecheow@gmail.com>
2015-06-05 12:33:43 +0800

    Initial Commit

---
+++ Combox/Combox.h
@@ -0,0 +1,19 @@
+//
+//  Combox.h
+//  Combox
+//
+//  Created by Ryne Cheow on 5/6/15.
+//  Copyright (c) 2015 Pointwelve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Combox.
+FOUNDATION_EXPORT double ComboxVersionNumber;
+
+//! Project version string for Combox.
+FOUNDATION_EXPORT const unsigned char ComboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Combox/PublicHeader.h>
+
+
---
+++ ComboxTests/ComboxTests.m
@@ -0,0 +1,40 @@
+//
+//  ComboxTests.m
+//  ComboxTests
+//
+//  Created by Ryne Cheow on 5/6/15.
+//  Copyright (c) 2015 Pointwelve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ComboxTests : XCTestCase
+
+@end
+
+@implementation ComboxTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
